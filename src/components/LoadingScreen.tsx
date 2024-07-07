700ac13b315256859ddc92698e25c43913a1de49
Sai Pratap <pratap@illiquidlabs.com>
2024-07-07 12:11:24 +0530

    load

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import styles from "./LoadingScreen.module.css";
 const LoadingScreen: React.FC = () => {
   return (
     <div className={styles.loadingContainer}>
-      <img src="/public/new-buy-2.gif" className={styles.backgroundGif} alt="Loading" />
+      <img src="/new-buy-2.gif" className={styles.backgroundGif} alt="Loading" />
       <div className={styles.loadingBar}></div>
     </div>
   );
